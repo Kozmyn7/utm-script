328531846f13d5317a812f29fa9890a289bf3344
Kozmyn7 <kozmyn7@gmail.com>
2024-07-28 23:17:56 +0300

    Update utm.js

--- utm.js
+++ utm.js
@@ -1,20 +1,24 @@
 // scripts.js
 
-console.log('Script loaded')
+console.log('Script Loaded');
 
 // Function to safely get a URL parameter
 function getQueryParam(param) {
+    console.log(`Getting query parameter: ${param}`);
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
 }
 
 // Function to safely set the value of a hidden input field
 function setHiddenFieldValue() {
+    console.log('Setting hidden field value');
     const utmSource = getQueryParam('utm_source');
+    console.log(`UTM Source: ${utmSource}`);
     const hiddenField = document.querySelector('input[name="sursa trafic"]');
     
     if (hiddenField && utmSource) {
         hiddenField.value = decodeURIComponent(utmSource); // Ensure value is properly decoded
+        console.log(`Hidden field value set to: ${hiddenField.value}`);
     } else {
         console.error('Hidden field not found or URL parameter is missing');
     }
